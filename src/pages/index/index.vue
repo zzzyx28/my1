987b898b378b531853cf70606a24eae217f303e1
zhengyx <1291870371@qq.com>
2023-11-10 17:33:22 +0800

    c2

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,4 +1,9 @@
 <template>
+	
+	<uni-card title="基础卡片" extra="额外信息">
+		<text>这是一个基础卡片示例，此示例展示了一个标题加标题额外信息的标准卡片。</text>
+	</uni-card>
+
   <view class="content">
     <image class="logo" src="/static/logo.png" />
     <view class="text-area">
